ba53c9423ac07350c394c0fa6846400c578b089f
francoriquelme <francoestebanru@gmail.com>
2022-03-25 19:06:00 -0300

    finished: Iterador.rb

---
+++ Iterador.rb
@@ -0,0 +1,15 @@
+# En el siguiente código, reemplazar la instrucción while por times dentro del programa
+# llamado iterador.rb.
+
+# el siguiente codigo
+
+# i = 0
+# while i < 50
+#     puts "Iteración #{i}"
+#     i = i + 1
+# end
+
+50.times do |i|
+    puts "Iteración #{i}"
+end
+
